9677dafe30a507c096d561fa2f7b2340389f1592
juanCoder64 <jgama12569@gmail.com>
2022-12-23 20:08:50 -0600

    add color invertion option

--- screen.h
+++ screen.h
@@ -3,7 +3,18 @@
 
 #ifndef SCREEN_H
 #define SCREEN_H
-
+#define INVERTED true
+#if INVERTED 
+#define BLACK 0xFFFF
+#define WHITE 0x0000
+#define BLUE 0xF800
+#define GREEN  0xF81F
+#define RED 0x07FF
+#define YELLOW  0x001F
+#define MAGENTA 0x07E0
+#define CYAN 0xFFE0
+#define ORANGE 0xFC00
+#else
 #define BLACK 0x0000
 #define WHITE 0xFFFF
 #define BLUE 0xF800
@@ -13,6 +24,7 @@
 #define MAGENTA 0xF81F
 #define CYAN 0xFFE0
 #define ORANGE 0xFC00
+#endif
 
 class screen {
 public:
@@ -28,8 +40,8 @@ public:
   void circle(float x, float y, uint8_t radius, uint16_t color);
   void line(uint8_t x, uint8_t y, uint8_t w, uint16_t color);
   void fontsize(int n);
-  uint8_t height = 128;
-  uint8_t width = 160;
+  uint32_t height = 80;
+  uint32_t width = 160;
   int menu(const char* name, const char* options);
   void centerText(int16_t y, String message, uint16_t color);
   void triangle(float x, float y, uint8_t radius, uint16_t color);
